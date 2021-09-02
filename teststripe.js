804db559516d205977ccdaf51ec3ce1fc1324b4f
hiva.2020 <taha_askari@yahoo.com>
2021-09-02 15:08:20 +0430

    comment stripe2

--- teststripe.js
+++ teststripe.js
@@ -1,6 +1,9 @@
 // This is test for stripe
-const stripe = require('stripe')('sk_test_51JUxBWJ14q5yQER3oCXkgvqpPzavujStTyUalgkdTS2fnLpKluJiSAMXe1B6LJxZj4J4rNLaD9fqeHtlA3LPm0yO00QYJQJBz9');
+const stripe = require('stripe')(
+  'sk_test_51JUxBWJ14q5yQER3oCXkgvqpPzavujStTyUalgkdTS2fnLpKluJiSAMXe1B6LJxZj4J4rNLaD9fqeHtlA3LPm0yO00QYJQJBz9'
+);
 const express = require('express');
+
 const app = express();
 app.use(express.static('public'));
 
@@ -14,17 +17,13 @@ app.post('/create-checkout-session', async (req, res) => {
         quantity: 1,
       },
     ],
-    payment_method_types: [
-      'card',
-      'bancontact',
-      'sofort',
-    ],
+    payment_method_types: ['card', 'bancontact', 'sofort'],
     mode: 'payment',
     success_url: `${YOUR_DOMAIN}/success.html`,
     cancel_url: `${YOUR_DOMAIN}/cancel.html`,
   });
 
-  res.redirect(303, session.url)
+  res.redirect(303, session.url);
 });
 
 app.listen(4242, () => console.log('Running on port 4242'));
