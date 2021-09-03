b2f6a4f0da2932e1272ad8e717acc6b9e6b60911
hiva.2020 <taha_askari@yahoo.com>
2021-09-03 10:32:31 +0430

    cores, stripe

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -27,6 +28,15 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) Global middlewares
+// Implement CORS
+app.use(cors());
+// Access-control-allow-origin *
+//api.natours.com
+// app.use(
+//   cors({
+//     origin: 'https://www.natours.com',
+//   })
+// );
 
 // serving static files
 app.use(express.static(path.join(__dirname, 'public')));
--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -9,7 +9,25 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.tourId);
   console.log(stripe);
   //2) create checkout session
-
+  console.log({
+    paymment_method_types: ['card'],
+    success_url: `${req.protocol}://${req.get('host')}/?tour=${
+      req.params.tourId
+    }&user=${req.user.id}&price=${tour.price}`,
+    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    client_reference_id: req.params.tourId,
+    line_items: [
+      {
+        name: `${tour.name} Tour`,
+        description: `${tour.summary}`,
+        amount: tour.price * 100,
+        currency: 'usd',
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+  });
   const session = await stripe.checkout.sessions.create({
     paymment_method_types: ['card'],
     success_url: `${req.protocol}://${req.get('host')}/?tour=${
@@ -21,17 +39,18 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     line_items: [
       {
         name: `${tour.name} Tour`,
-        description: tour.summary,
-        images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
+        description: `${tour.summary}`,
         amount: tour.price * 100,
         currency: 'usd',
         quantity: 1,
       },
     ],
     mode: 'payment',
-    stripe_account: 'acct_1I429QACVO0I2rXH',
   });
 
+  
+  //   stripe_account: 'acct_1I429QACVO0I2rXH',
+  //  images: [`https://www.natours.dev/img/tours/${tour.imageCover}`],
   //3) create session as response
   res.status(200).json({
     status: 'success',
