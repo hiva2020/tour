74d87c6512a3da80725b9fedf047834d0f5dcf36
hiva.2020 <taha_askari@yahoo.com>
2021-09-03 12:40:47 +0430

    Improved Stripe  implementation

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,5 +1,6 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
 const Booking = require('../models/bookingModel');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
@@ -9,30 +10,12 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   const tour = await Tour.findById(req.params.tourId);
   console.log(stripe);
   //2) create checkout session
-  console.log({
-    payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/?tour=${
-      req.params.tourId
-    }&user=${req.user.id}&price=${tour.price}`,
-    cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
-    customer_email: req.user.email,
-    client_reference_id: req.params.tourId,
-    line_items: [
-      {
-        name: `${tour.name} Tour`,
-        description: `${tour.summary}`,
-        amount: tour.price * 100,
-        currency: 'usd',
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-  });
   const session = await stripe.checkout.sessions.create({
     payment_method_types: ['card'],
-    success_url: `${req.protocol}://${req.get('host')}/?tour=${
-      req.params.tourId
-    }&user=${req.user.id}&price=${tour.price}`,
+    // success_url: `${req.protocol}://${req.get('host')}/?tour=${
+    //   req.params.tourId
+    // }&user=${req.user.id}&price=${tour.price}`,
+    success_url: `${req.protocol}://${req.get('host')}/my-tours`,
     cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
     customer_email: req.user.email,
     client_reference_id: req.params.tourId,
@@ -60,17 +43,66 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
   });
 });
 
-exports.createBookingCheckout = catchAsync(async (req, res, next) => {
-  // This is  only Temperory , because its unsecure : everyone  can make booking without paying
-  const { tour, user, price } = req.query;
+// exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+//   // This is  only Temperory , because its unsecure : everyone  can make booking without paying
+//   const { tour, user, price } = req.query;
 
-  if (!tour && !user && !price) return next();
+//   if (!tour && !user && !price) return next();
 
-  await Booking.create({ tour, user, price });
+//   await Booking.create({ tour, user, price });
 
-  res.redirect(req.originalUrl.split('?')[0]);
-});
+//   res.redirect(req.originalUrl.split('?')[0]);
+// });
 
+exports.createBookingCheckout =async session =>  {
+  const tour = session.client_reference_id;
+  const user =(await User.findOne({email: session.customer_email})).id;
+  const price =session.line_items[0].amount/100;
+
+   await Booking.create({ tour, user, price });
+  //res.redirect(req.originalUrl.split('?')[0]);
+};
+
+exports.webhookCheckout = (req, res, next) => {
+  const signature = req.headers['stripe-signature']; 
+  let event;
+  try {
+    try {
+      event = stripe.webhooks.constructEvent(req.body, signature, process.env.STRIPE_WEBHOOK_SECRET);
+    } catch (err) {
+     return res.status(400).send(`Webhook Error: ${err.message}`);
+    
+    }
+    // Handle the event
+  switch (event.type) {
+    case 'checkout.session.async_payment_failed':
+      const checkout = event.data.object;
+      // Then define and call a function to handle the event checkout.session.async_payment_failed
+      break;
+    case 'checkout.session.async_payment_succeeded':
+      const checkout = event.data.object;
+
+      // Then define and call a function to handle the event checkout.session.async_payment_succeeded
+      break;
+    case 'checkout.session.completed':
+      const checkout = event.data.object;
+      this.createBookingCheckout(event.data.object);
+      // Then define and call a function to handle the event checkout.session.completed
+      res.status(200).json({received: true});
+      break;
+    case 'payment_intent.amount_capturable_updated':
+      const paymentIntent = event.data.object;
+      // Then define and call a function to handle the event payment_intent.amount_capturable_updated
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+  } catch (err) {
+    return res.status(400).send(`Webhook Error: ${err.message}`);
+  }
+ 
+};
 exports.createBooking = factory.createOne(Booking);
 exports.getBooking = factory.getOne(Booking);
 exports.getAllBookings = factory.getAll(Booking);
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -7,18 +7,18 @@ const router = express.Router();
 
 router.use(authController.isLoggedIn);
 
-router.get(
-  '/',
-  bookingController.createBookingCheckout,
-  authController.isLoggedIn,
-  viewController.getOverview
-);
+router.get('/', authController.isLoggedIn, viewController.getOverview);
 
 router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
 
 router.get('/login', authController.isLoggedIn, viewController.getLoginForm);
 router.get('/me', authController.protect, viewController.getAccount);
-router.get('/my-tours', authController.protect, viewController.getMyTours);
+router.get(
+  '/my-tours',
+  authController.protect,
+  //bookingController.createBookingCheckout,
+  viewController.getMyTours
+);
 
 // router.post(
 //   '/submit-user-data',
